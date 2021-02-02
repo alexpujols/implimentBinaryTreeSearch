f5ca67a354c9b87d369f8de6baf6e3635fa6cac6
Alex <alex.pujols@gmail.com>
2021-02-01 18:37:32 -0600

    added txt file and gitignore

--- implimentBinaryTreeSearch.py
+++ implimentBinaryTreeSearch.py
@@ -1,6 +1,6 @@
 ##!/usr/bin/env python
 __author__ = "Alex Pujols"
-__copyright__ = "Copyright 2021, Evaluating Searching Algorithms Project TIM-6110 wk6"
+__copyright__ = "Copyright 2021, Evaluating Searching Algorithms Project TIM-6110 wk7"
 __credits__ = ["Alex Pujols"]
 __license__ = "GPL"
 __version__ = "1.0"
