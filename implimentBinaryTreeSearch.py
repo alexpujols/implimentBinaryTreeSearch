a5ad1c209dec6427e7802cfb27d09e624ea60e97
Alex <alex.pujols@gmail.com>
2021-02-01 18:26:24 -0600

    first commit

---
+++ implimentBinaryTreeSearch.py
@@ -0,0 +1,9 @@
+##!/usr/bin/env python
+__author__ = "Alex Pujols"
+__copyright__ = "Copyright 2021, Evaluating Searching Algorithms Project TIM-6110 wk6"
+__credits__ = ["Alex Pujols"]
+__license__ = "GPL"
+__version__ = "1.0"
+__maintainer__ = "Alex Pujols"
+__email__ = "alex.pujols@gmail.com"
+__status__ = "Prototype"
