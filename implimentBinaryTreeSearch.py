e6bd0e0602ef708f4d847f01c810bb58e4c794a5
Alex <alex.pujols@gmail.com>
2021-02-04 18:53:37 -0600

    adding code to read in, and parse, new file

--- implimentBinaryTreeSearch.py
+++ implimentBinaryTreeSearch.py
@@ -1,9 +1,58 @@
 ##!/usr/bin/env python
+'''
+                      ::::::
+                    :+:  :+:
+                   +:+   +:+
+                  +#++:++#++:::::::
+                 +#+     +#+     :+:
+                #+#      #+#     +:+
+               ###       ###+:++#""
+                         +#+
+                         #+#
+                         ###
+'''
 __author__ = "Alex Pujols"
-__copyright__ = "Copyright 2021, Evaluating Searching Algorithms Project TIM-6110 wk7"
+__copyright__ = "TBD"
 __credits__ = ["Alex Pujols"]
 __license__ = "GPL"
 __version__ = "1.0"
 __maintainer__ = "Alex Pujols"
 __email__ = "alex.pujols@gmail.com"
 __status__ = "Prototype"
+
+'''
+What does this code do?
+'''
+
+# Import modules declarations
+import string
+import json
+
+# Function declarations
+
+# Function for TBD
+
+# Function for TBD
+
+# Function for TBD
+
+# Function for TBD
+
+# Function for TBD
+
+
+# Main code begins
+_filename_ = 'data.json'
+
+# Open the file data.json and read from it
+with open(_filename_) as f:
+    data = json.load(f)
+# Create a dictionary index with the value "Name" and store in new list "names"
+names = []
+for i in data:
+    name = i['Name']
+    names.append(name)
+
+# Set global vaiables
+
+# Begin main code execution
