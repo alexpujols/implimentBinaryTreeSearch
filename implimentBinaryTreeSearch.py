9f36a92cba5578ea230fd5bcced0bfdbb6d20216
Alex <alex.pujols@gmail.com>
2021-02-06 09:38:13 -0600

    New header information and class creation

--- implimentBinaryTreeSearch.py
+++ implimentBinaryTreeSearch.py
@@ -28,7 +28,47 @@ What does this code do?
 import string
 import json
 
-# Function declarations
+## Class declarations
+
+# Class for TBD
+class Node:
+    """ A class that models attributes of people """
+
+    def __init__(self, data):
+        """ Initialize data categories """
+        self.left = None
+        self.right = None
+        self.data = data
+    # Method to insert new value into tree - inorder method
+    def Insert(self, data):
+        if self.data:                       # If parent node exists
+            if data < self.data:            ## If value is less than parent node
+                if self.left is None:       ### If left node is empty
+                    self.left = Node(data)  #### Assign data value to left node
+                else:                       ### If left node is NOT empty
+                    self.left.Insert(data)  #### recurrsively left.insert data until empty left node is found
+            elif data > self.data:          ## If value is greater than parent node
+                if self.right is None:      ### If right node is empty
+                    self.right = Node(data) #### Assign data value to right node
+                else:                       ### If right node is NOT empty
+                    self.right.Insert(data) #### recurrsively right.insert data until a value greater than parent node is encountered
+        else:                               # If parent node does NOT exist
+            self.data = data                ## Assign data value to parent node
+    # Method to print tree values
+    def PrintTree(self):
+        if self.left:                       # If a left node value exists
+            self.left.PrintTree()           ## recurrsively move left until at leftmost node
+        print(self.data)                    # Print value active node data
+        if self.right:                      # If right node value exists
+            self.right.PrintTree()          ## recurrsively move right until at rightmost node
+
+
+
+# Class for TBD
+
+# Class for TBD
+
+## Function declarations
 
 # Function for TBD
 
@@ -41,18 +81,25 @@ import json
 # Function for TBD
 
 
-# Main code begins
+### Main code begins ###
+
+# Set global vaiables
 _filename_ = 'data.json'
+
+# Begin main code execution
 
 # Open the file data.json and read from it
 with open(_filename_) as f:
     data = json.load(f)
-# Create a dictionary index with the value "Name" and store in new list "names"
+# Initialize arrays/class for data intake
 names = []
+people = Node(None)
+# Read-in all JSON values
 for i in data:
     name = i['Name']
     names.append(name)
+    people.Insert(name)
 
-# Set global vaiables
+people.PrintTree()
 
-# Begin main code execution
+## Main code ends ###
